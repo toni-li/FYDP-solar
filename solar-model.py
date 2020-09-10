6ade9c288eece48961dc70cb9fbea79b5ce2a25b
tanushree-sharma <tanushree@hotmail.ca>
2020-09-09 23:04:50 -0400

    add-constraints

--- solar-model.py
+++ solar-model.py
@@ -25,10 +25,9 @@ else:
     else:
         E = [[E0, (E0*1.3), E0, E0]]
 
-print(E[0][0])
 G = [[0.00017952, 0.00017952, 0.00017952, 0.00017952]] # cost of electricity from the grid at year 0($/Wh)
 m = [[2.5,2.5,2.5,2.5]]  # yearly maintenance cost ($/panel)
-B = 10000  # budget from user
+B = 15000  # budget from user
 C = 315*2.80  # cost of each solar panel ($/panel) (12 modules of 60cell)
 Ap = 18.9  # area of solar panel (ft^2) (40 * 68 inches)
 Ar = 1700  # area of the roof (ft^2) from user
@@ -73,7 +72,7 @@ availability = 0.03
 # how much energy can be used (%) (factors: irradiation, shadow, direction)
 U = ((1 - soiling) * (1 - shading) * (1 - snow) * (1 - mismatch) * (1 - wiring) * (1 - connections) * (
         1 - nameplaterating) * (1 - availability))
-print(U)
+
 # convert m into present value (remain constant throughout seasons)
 i = 0.00206
 for t in range(1,T):
@@ -93,17 +92,18 @@ H_3 = Ha + ((Sh[3] - Avg_Sh) * (Ha / Avg_Sh))
 
 H = [H_0, H_1, H_2, H_3]
 
+#DELETE
 # find max of all seasons using E @ t=0
 # number of solar panels needed to fulfill at least 100% of electricity from the grid
-P_0 = math.ceil((E[0][0] / L[0]) / (P * H[0] *24 * U) * 0.35)
-P_1 = math.ceil((E[0][1] / L[1]) / (P * H[1] *24 * U) * 0.35)
-P_2 = math.ceil((E[0][2] / L[2]) / (P * H[2] *24 * U) * 0.35)
-P_3 = math.ceil((E[0][3] / L[3]) / (P * H[3] *24 * U) * 0.35)
- 
-Pn = max(P_0, P_1, P_2, P_3)
+#P_0 = math.ceil((E[0][0] / L[0]) / (P * H[0] *24 * U) * 0.35)
+#P_1 = math.ceil((E[0][1] / L[1]) / (P * H[1] *24 * U) * 0.35)
+#P_2 = math.ceil((E[0][2] / L[2]) / (P * H[2] *24 * U) * 0.35)
+#P_3 = math.ceil((E[0][3] / L[3]) / (P * H[3] *24 * U) * 0.35)
+#Pn = max(P_0, P_1, P_2, P_3)
 
-print(P_0, P_1, P_2)
-print(max(P_0, P_1, P_2))
+#Pn = [P_0, P_1, P_2, P_3]
+
+
                             
 # initializing model
 model = Model()
@@ -117,8 +117,14 @@ model.objective = minimize(xsum((E[t][s] - ((y * P * H[s] * 24 * U * L[s]) * (1 
 # adding constraints
 model += (y * C) + F <= B  # budget constraint
 model += y * Ap <= Armax  # area of roof constraint 
-model += Pn - y >= 0  # fulfill demand constraint
+# DELETE
+#model += Pn - y >= 0  # fulfill demand constraint
 model += y >= 0  # non-negativity constraint
+# can't generate more electricity than needed each season in year 24
+model += (E[24][0] - ((y * P * H[0] * 24 * U * L[0]) * (1 - d[24][0])))>= 0
+model += (E[24][1] - ((y * P * H[1] * 24 * U * L[1]) * (1 - d[24][1]))) >= 0 
+model += (E[24][2] - ((y * P * H[2] * 24 * U * L[2]) * (1 - d[24][2]))) >= 0 
+model += (E[24][3] - ((y * P * H[3] * 24 * U * L[3]) * (1 - d[24][3]))) >= 0
 
 # solving the MIP
 status = model.optimize()
@@ -137,7 +143,7 @@ if status == OptimizationStatus.OPTIMAL or status == OptimizationStatus.FEASIBLE
     print("Total Capital Cost: $" + str(totalCost))
 if status == OptimizationStatus.NO_SOLUTION_FOUND:
     print("no feasible solution :(")
-
+'''
 # yearly grid energy cost w/o solar vs. yearly grid energy cost w/ solar (grouped bar chart)
 # set width of bar
 barWidth = 0.40
@@ -258,3 +264,4 @@ plt.title('Carbon Footprint Tradeoff over 25 years')
 
 plt.show()
 
+'''
